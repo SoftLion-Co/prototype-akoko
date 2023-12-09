5cc7a08cd04b8fe0e9ef13520f7a991138084a7d
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2023-12-09 15:08:18 +0200

    feature: add basic font style

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,13 @@
 export default function Home() {
   return (
     <div>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure vel
+        deserunt magnam! Perspiciatis corporis neque voluptatum, temporibus,
+        placeat ea officiis cumque ipsa quibusdam animi aspernatur omnis facilis
+        ad, expedita voluptate.
+      </p>
     </div>
-  )
+  );
 }
