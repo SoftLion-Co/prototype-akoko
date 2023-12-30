09deacd1fc37c59b710aab414653199a7a262571
Paul <115890528+PaulGraur@users.noreply.github.com>
2023-12-30 16:31:43 +0200

    feature: add responsive

--- components/FooterComponent.tsx
+++ components/FooterComponent.tsx
@@ -95,9 +95,9 @@ const FooterComponent = () => {
 
   return (
     <footer className="bg-primary-footer">
-      <div className="container flex flex-col gap-36 py-20">
-        <div className="flex justify-between">
-          <div className="flex gap-12">
+      <div className="container flex flex-col gap-[50px] py-20 tablet:gap-[200px]">
+        <div className="flex flex-wrap laptop:flex-nowrap justify-between">
+          <div className="flex mobile:flex-wrap gap-[40px] mobile:justify-between mb-[60px] tablet:gap-[84px] laptop:gap-[40px]">
             <div className="flex flex-col gap-9">
               <p className="text-primary">Women</p>
               <div className="flex flex-col gap-1">
@@ -140,42 +140,48 @@ const FooterComponent = () => {
             </div>
           </div>
 
-          <div>
-            <p className="text-primary text-base font-w-500 mb-10">
-              Будь в курсі новинок
+          <div className="flex flex-col flex-col-reverse items-start tablet:gap-[370px] tablet:flex-row tablet:items-end laptop:items-start">
+            <p className="text-primary mobile:block tablet:block laptop:hidden">
+              © AKOKO 2024
             </p>
-            <div className="relative mb-20">
-              <input
-                className="w-96 h-16 w-full px-4 py-2 border-gray-300 focus:border-gray-500 focus:outline-none"
-                placeholder="Пошта"
-                type="text"
-                value={email}
-                onChange={handleEmailChange}
-              />
-              <div className="absolute inset-y-0 right-2 flex items-center ">
-                <div className="border border-gray-500 bg-black">
-                  <button
-                    className="w-14 h-11 rounded-md bg-gray-800 text-primary"
-                    onClick={handleSubscribeClick}
-                  >
-                    +
-                  </button>
+
+            <div className="mobile:mb-[50px]">
+              <p className="text-primary text-base font-w-500 mobile:mb-[20px] tablet:mb-[28px] laptop:mb-[32px] desktop:mb-[38px]">
+                Будь в курсі новинок
+              </p>
+              <div className="relative mobile:mb-[46px] tablet:mb-[60px] laptop:mb-[68px] desktop:mb-[86px]">
+                <input
+                  className="px-4 py-2 border-gray-300 focus:border-gray-500 focus:outline-none mobile:w-[200px] h-[34px] tablet:w-[254px] tablet:h-[42px] laptop:w-[304px] laptop:h-[50px] desktop:w-[362px] desktop:h-[60px]"
+                  placeholder="Пошта"
+                  type="text"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+                <div className="absolute inset-y-0 right-2 flex items-center ">
+                  <div className="border border-gray-500 bg-black">
+                    <button
+                      className="rounded-md bg-gray-800 text-primary w-[32px] h-[24px] tablet:w-[40px] tablet:h-[30px] laptop:w-[48px] laptop:h-[36px] desktop:w-[56px] desktop:h-[44px]"
+                      onClick={handleSubscribeClick}
+                    >
+                      +
+                    </button>
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <div className="flex gap-5">
-              {SocialNetworks.map((item, index) => (
-                <Link key={index} href={item.href} target="_blank">
-                  <Image src={item.social || ""} alt={item.name} />
-                </Link>
-              ))}
+              <div className="flex gap-5">
+                {SocialNetworks.map((item, index) => (
+                  <Link key={index} href={item.href} target="_blank">
+                    <Image src={item.social || ""} alt={item.name} />
+                  </Link>
+                ))}
+              </div>
             </div>
           </div>
         </div>
 
-        <div className="flex gap-80">
-          <p className="text-primary">© AKOKO 2024</p>
+        <div className="flex mobile:flex-col items-center justify-center gap-[110px] tablet:gap-[200px] laptop:gap-[240px] laptop:items-start laptop:justify-start desktop:gap-[345px]">
+          <p className="text-primary hidden laptop:block">© AKOKO 2024</p>
           <Image src={Logo} alt="Logo" />
         </div>
       </div>
