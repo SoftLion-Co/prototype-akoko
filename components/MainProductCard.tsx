49fc7add7e1a944d973a9af7c5b7f0990a6b0c73
Paul <115890528+PaulGraur@users.noreply.github.com>
2023-12-31 20:25:18 +0200

    Create MainProductCard.tsx

---
+++ components/MainProductCard.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const MainProductCard = () => {
+  return (
+    <div>
+      <Image src={""} alt="Product"/>
+
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <p></p>
+    </div>
+  );
+};
+
+export default MainProductCard;
