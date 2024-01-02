bcc0a80317780813eded150e68f498ff0576721d
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-01-02 11:14:54 +0200

    feature: add props and ligic for it

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,16 +1,65 @@
-export default function Home() {
+import React from "react";
+import MainProductCard from "@/components/MainProductCard";
+
+interface ProductCardData {
+  productImg: string;
+  name: string;
+  color: string[];
+  price: string;
+}
+
+const testProducts: ProductCardData[] = [
+  {
+    productImg:
+      "https://content1.rozetka.com.ua/goods/images/original/301736389.jpg",
+    name: "Sample Product 1 Sample Product 1 Sample Product 1",
+    color: ["#00", "#00"],
+    price: "$29.99",
+  },
+  {
+    productImg:
+      "https://content1.rozetka.com.ua/goods/images/original/301736389.jpg",
+    name: "Sample Product 2",
+    color: ["#00", "#00"],
+    price: "$39.99",
+  },
+  {
+    productImg:
+      "https://content1.rozetka.com.ua/goods/images/original/301736389.jpg",
+    name: "Sample Product 3",
+    color: ["#00", "#00"],
+    price: "$26.99",
+  },
+];
+
+const Home: React.FC = () => {
   return (
-    <section className="w-full ">
+    <section className="w-full">
       <div className="container">
         <h1 className="font-w-300">Hello world</h1>
-        <h2>Для жІнок</h2>
+        <h2>Для жінок</h2>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure vel
           deserunt magnam! Perspiciatis corporis neque voluptatum, temporibus,
           placeat ea officiis cumque ipsa quibusdam animi aspernatur omnis
           facilis ad, expedita voluptate.
         </p>
+
+        <div className="flex gap-[55px] flex-basis-[calc(100%/3)] w-[calc(100%/3)]">
+          {testProducts.map((product, index) => (
+            <MainProductCard
+              classNames="flex"
+              key={index}
+              productImg={product.productImg}
+              name={product.name}
+              color={product.color}
+              price={product.price}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
-}
+};
+
+export default Home;
--- components/MainProductCard.tsx
+++ components/MainProductCard.tsx
@@ -1,54 +1,70 @@
+import React from "react";
 import Image from "next/image";
-
 import Cart from "@/images/icons/cart.svg";
 import Wish from "@/images/icons/wish.svg";
 
 interface ProductCardData {
-  productImg: string;
+  productImg?: string;
   name: string;
-  color: string[];
-  price: string;
+  color?: string[];
+  price?: string;
+  availability?: string;
+  classNames?: string;
 }
-
-const testProducts: ProductCardData[] = [
-  {
-    productImg:
-      "https://content1.rozetka.com.ua/goods/images/original/301736389.jpg",
-    name: "Sample Product 1",
-    color: ["#ff0000", "#ff0000"],
-    price: "$29.99",
-  },
-  {
-    productImg:
-      "https://content1.rozetka.com.ua/goods/images/original/301736389.jpg",
-    name: "Sample Product 2",
-    color: ["#0000ff", "#ff0000"],
-    price: "$39.99",
-  },
-];
 
 const MainProductCard: React.FC<ProductCardData> = ({
   productImg,
   name,
   color,
   price,
+  availability,
+  classNames,
+  ...restProps
 }) => {
+  const hasAdditionalContent = color && color.length > 0 && price;
+
   return (
-    <div className="">
-      <Image className="" src={productImg} alt="Product" />
+    <div className={`flex flex-col ${classNames}`} {...restProps}>
+      <Image
+        className="border-[1px] border-black mb-[24px]"
+        src={""}
+        alt="Product"
+        width={315}
+        height={420}
+      />
+      <div className="h-[66px] bg-[#272727]"></div>
 
-      <p>{name}</p>
+      <p
+        className={`text-[18px] h-[60px] mb-[20px] ${
+          !hasAdditionalContent && "flex justify-center text-center"
+        }`}
+      >
+        {name}
+      </p>
 
-      <div>
-        <div className="color"></div>
+      {hasAdditionalContent && (
+        <div>
+          <div className="flex justify-between mb-[24px]">
+            <div className="flex gap-[10px] ">
+              {color.map((c, index) => (
+                <div
+                  className="border-[1px] border-black rounded-[50%]"
+                  key={index}
+                >
+                  {c}
+                </div>
+              ))}
+            </div>
 
-        <div className="flex">
-          <Image src={Cart} alt="Cart" />
-          <Image src={Wish} alt="Wish" />
+            <div className="flex gap-[10px]">
+              <Image src={Cart} alt="Cart" height={25} />
+              <Image src={Wish} alt="Wish" height={30} />
+            </div>
+          </div>
+
+          <p>Ціна: {price}</p>
         </div>
-      </div>
-
-      <p>{price}</p>
+      )}
     </div>
   );
 };
