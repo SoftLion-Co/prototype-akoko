3c7cd516adfbdc365f209a2fd9889a4f377997eb
Paul <115890528+PaulGraur@users.noreply.github.com>
2024-01-03 15:18:56 +0200

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,8 @@
 import CollectionSlider from "@/sections/home_page/CollectionSlider/CollectionSlider";
-import MainTitleComponent from "@/components/MainTitleComponent";
 
 export default function Home() {
   return (
     <>
-      <MainTitleComponent title="New Collection" levelTitle={1} />
       <CollectionSlider />
     </>
   );
