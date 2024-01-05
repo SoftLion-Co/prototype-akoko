23b1239f6e07ab01e8868af94749f1ae05206b48
Yurii <tahkict.yup4ik@gmail.com>
2024-01-05 13:17:07 +0200

    feature: blog card hover
    -Залишилось допрацювати логіку обмеження заголовку та тексту трьома крапками

--- components/blog_page/BlogCardComponent.tsx
+++ components/blog_page/BlogCardComponent.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, { useRef, useEffect, useState } from "react";
 import { Link } from "@/navigation";
 import Image from "next/image";
 
@@ -6,16 +7,38 @@ type BlogCardComponentProps = {
   blogId: string;
   title: string;
   imageSrc: string;
+  text: string;
 };
 
-const BlogCardComponent = ({
-  blogId,
-  title,
-  imageSrc,
-}: BlogCardComponentProps) => {
+const BlogCardComponent = ({ blogId, title, text, imageSrc }: BlogCardComponentProps) => {
+  const titleRef = useRef(null);
+  const [isHovered, setIsHovered] = useState(false);
+
+  useEffect(() => {
+    const titleElement = titleRef.current;
+
+    if (titleElement) {
+      const lineHeight = parseInt(window.getComputedStyle(titleElement).lineHeight);
+      const maxLines = 2;
+
+      if (titleElement.offsetHeight > lineHeight * maxLines) {
+        titleElement.classList.add("truncate");
+      }
+    }
+  }, []);
+
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleLeave = () => {
+    setIsHovered(false);
+  };
+
   return (
     <Link href={`/blog/${blogId}`}>
-      <div className="relative">
+      <div className={`relative group overflow-hidden ${isHovered ? "whitespace-normal" : ""}`}onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}>
         <Image
           src={imageSrc}
           alt="blog image"
@@ -23,8 +46,17 @@ const BlogCardComponent = ({
           height={1000}
           className="aspect-[53/55] object-cover"
         />
-        <div className="absolute flex items-center h-10 bottom-[19%] bg-[#00000070] pr-3">
-          <h2 className="text-primary text-lg font-extralight">{title}</h2>
+        <div
+          className={`absolute bottom-0 bg-primary/75 h-[28%] group-hover:h-[60%] w-full py-3 laptop:py-6 desktop:py-7 px-1 laptop:px-2 desktop:px-[10px] transition-all duration-300`}
+          
+        >
+          <h2
+            ref={titleRef}
+            className={`text-xl font-extralight laptop:text-3xl desktop:text-4xl mb-2 ${isHovered ? "" : "whitespace-nowrap overflow-hidden max-h-[2.5em] truncate"}`}
+          >
+            {title}
+          </h2>
+          <p className="text-sm font-light opacity-0 group-hover:opacity-100 transition-opacity duration-300 absolute left-0 right-0 px-1 laptop:px-2 desktop:px-[10px]">{text}</p>
         </div>
       </div>
     </Link>
--- sections/blog_page/BlogPageSection.tsx
+++ sections/blog_page/BlogPageSection.tsx
@@ -2,53 +2,56 @@ import React from "react";
 import BlogCardComponent from "@/components/blog_page/BlogCardComponent";
 
 const BlogPage = () => {
-  const blogPosts = [
+  const blogData = [
     {
-      id: "234",
-      title: "Назва статті",
-      imageSrc:
-        "https://deadline.com/wp-content/uploads/2023/12/angelina-jolie.jpg",
+      id: "1",
+      title: "Топ-5 модних трендів весна-літо 2024 від AKOKO",
+      text: "Дізнайтеся про найгарячіші модні тренди сезону весна-літо 2024 від AKOKO. Від барвистих кольорів до елегантних фасонів, ми розкажемо вам про все, що трендово цього сезону.",
+      imageSrc: "https://deadline.com/wp-content/uploads/2023/12/angelina-jolie.jpg",
     },
     {
-      id: "567",
-      title: "Sample Blog Title 2",
-      imageSrc:
-        "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
+      id: "2",
+      title: "Як обрати ідеальний образ: поради від стилістів AKOKO",
+      text: "Отримайте поради від наших професійних стилістів про те, як підібрати ідеальний образ для будь-якої події. Наші експерти розкажуть вам, як виглядати стильно та впевнено в будь-якій ситуації.",
+      imageSrc: "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
     },
     {
-      id: "234",
-      title: "Назва статті",
-      imageSrc:
-        "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
+      id: "3",
+      title: "За лаштунками: нова колекція AKOKO",
+      text: "Поглибіться в атмосферу творчого процесу та дізнайтеся, як створюється нова колекція від AKOKO. Від ідеї до реалізації - кожна деталь має свою історію.",
+      imageSrc: "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
     },
     {
-      id: "567",
-      title: "Sample Blog Title 2",
-      imageSrc:
-        "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
+      id: "4",
+      title: "Аксесуари, які зроблять ваш образ неповторним",
+      text: "Відкрийте для себе унікальні аксесуари від AKOKO, які додають шарму та стилю вашому образу. Намагаємося робити ваш стиль неповторним та вишуканим.",
+      imageSrc: "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
     },
     {
-      id: "234",
-      title: "Назва статті",
-      imageSrc:
-        "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
+      id: "5",
+      title: "Весільна колекція від AKOKO: елегантність у кожній деталі",
+      text: "Оголошуємо про нову весільну колекцію від AKOKO. Вишукані сукні, ексклюзивні дизайни та найкращі тканини - ми робимо кожен момент вашого весілля особливим.",
+      imageSrc: "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
     },
     {
-      id: "567",
-      title: "Sample Blog Title 2",
-      imageSrc:
-        "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
+      id: "6",
+      title: "Тренди взуття: виберіть ідеальні пари для будь-якого образу",
+      text: "Погляньте на найгарячіші тренди взуття цього сезону від AKOKO. Від зручних кросівок до елегантних туфель - ми допоможемо вам знайти ідеальну пару для кожної обстановки.",
+      imageSrc: "https://png.pngtree.com/background/20230513/original/pngtree-dress-shop-offering-in-a-red-and-orange-color-fashion-showroom-picture-image_2507152.jpg",
     },
   ];
+  
+  
 
   return (
-    <div className="grid grid-cols-1 tablet:grid-cols-2 gap-y-[68px] tablet:gap-y-[76px] tablet:gap-x-[84px] laptop:gap-y-[128px] laptop:gap-x-[141px] desktop:gap-y-[161px] desktop:gap-x-[178px]">
-      {blogPosts.map((post) => (
+    <div className="grid grid-cols-1 gap-y-[60px] tablet:grid-cols-2 tablet:gap-y-[71px] tablet:gap-x-[64px] laptop:gap-y-[118px] laptop:gap-x-[108px] desktop:gap-y-[146px] desktop:gap-x-[134px]">
+      {blogData.map((post) => (
         <BlogCardComponent
           key={post.id}
           blogId={post.id}
           title={post.title}
           imageSrc={post.imageSrc}
+          text={post.text}
         />
       ))}
     </div>
