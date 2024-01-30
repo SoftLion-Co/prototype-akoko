de954fdff149318bc58bf11fedddd1fde7ad51ce
Yan Zhabko <113374407+yanzhabko@users.noreply.github.com>
2024-01-30 12:04:32 +0100

    feature? fix width card

--- components/StyleCardComponent.tsx
+++ components/StyleCardComponent.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Link } from "@/navigation";
+import classNames from "classnames";
 
 interface CartProps {
   name: string;
@@ -13,7 +14,9 @@ const StyleCardComponent: React.FC<CartProps> = ({ name, image, link }) => {
     <div className="w-full h-full flex flex-col gap-[8px] justify-between items-center tablet:w-auto tablet:h-auto">
       <Link href={link} className="h-full w-full">
         <Image
-          className="object-cover w-full h-[380px] tablet:w-[160px] tablet:h-[210px] laptop:w-[240px] laptop:h-[360px] desktop:min-w-[315px] desktop:h-[420px]"
+          className={classNames(
+            "object-cover min-w-[160px] w-full h-[340px] tablet:w-[160px] tablet:h-[210px] laptop:w-[240px] laptop:h-[360px] desktop:min-w-[315px] desktop:h-[420px]"
+          )}
           src={image}
           width={325}
           height={420}
